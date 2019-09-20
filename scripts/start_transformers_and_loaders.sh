bb1b89d19d189fd5bb7336c8f009967eb9ab6428
Jernej Makovsek <jernej.makovsek@gmail.com>
2019-09-20 15:20:36 +0200

    Fix script for production

--- scripts/start_transformers_and_loaders.sh
+++ scripts/start_transformers_and_loaders.sh
@@ -1,11 +1,9 @@
 #!/usr/bin/env bash
 
-# /opt/puma/puma-ETL/venv/bin/python3 /opt/puma/puma-ETL/src/extractor.py enp1s0 100
-/opt/puma/puma-ETL/venv/bin/python3 /opt/puma/puma-ETL/src/extractor.py enp111s0 10
-# /opt/puma/puma-ETL/venv/bin/python3 /opt/puma/puma-ETL/src/transformer_pcap.py &
-/opt/puma/puma-ETL/venv/bin/python3 /opt/puma/puma-ETL/src/transformer_pcap.py /home/toaster/staging/transformer_pcap &
-# /opt/puma/puma-ETL/venv/bin/python3 /opt/puma/puma-ETL/src/transformer_msgstorage.py &
-/opt/puma/puma-ETL/venv/bin/python3 /opt/puma/puma-ETL/src/transformer_msgstorage.py /home/toaster/staging/transformer_msgstorage &
-# /opt/puma/puma-ETL/venv/bin/python3 /opt/puma/puma-ETL/src/loader_backup.py /mnt/backup &
-/opt/puma/puma-ETL/venv/bin/python3 /opt/puma/puma-ETL/src/loader_backup.py /home/toaster/staging/backup /home/toaster/staging/loader_backup /home/toaster/staging/transformer_pcap /home/toaster/staging/transformer_msgstorage &
-/opt/puma/puma-ETL/venv/bin/python3 /opt/puma/puma-ETL/src/loader_archive.py /mnt/gdrive/project_data/puma-recorder /home/toaster/staging/loader_archive /home/toaster/staging/loader_backup &
+/opt/puma/puma-ETL/venv/bin/python3 /opt/puma/puma-ETL/src/extractor.py enp1s0 100
+/opt/puma/puma-ETL/venv/bin/python3 /opt/puma/puma-ETL/src/transformer_pcap.py &
+/opt/puma/puma-ETL/venv/bin/python3 /opt/puma/puma-ETL/src/transformer_msgstorage.py &
+/opt/puma/puma-ETL/venv/bin/python3 /opt/puma/puma-ETL/src/loader_backup.py /mnt/backup &
+#/opt/puma/puma-ETL/venv/bin/python3 /opt/puma/puma-ETL/src/loader_backup.py /home/toaster/staging/backup /home/toaster/staging/loader_backup /home/toaster/staging/transformer_pcap /home/toaster/staging/transformer_msgstorage &
+/opt/puma/puma-ETL/venv/bin/python3 /opt/puma/puma-ETL/src/loader_archive.py /mnt/gdrive/project_data/puma-recorder
+#/opt/puma/puma-ETL/venv/bin/python3 /opt/puma/puma-ETL/src/loader_archive.py /mnt/gdrive/project_data/puma-recorder /home/toaster/staging/loader_archive /home/toaster/staging/loader_backup &
