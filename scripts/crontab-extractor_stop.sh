6a3ecceb205ebe967fc6a5eb26ce26ea71ed10ca
Jernej Makovsek <jernej.makovsek@gmail.com>
2019-09-20 15:22:46 +0200

    Update crontab stop script

--- scripts/crontab-extractor_stop.sh
+++ scripts/crontab-extractor_stop.sh
@@ -11,8 +11,8 @@ pkill -2 ${RECORDER_PROCESS_NAME}
 sleep 10
 pkill -9 ${RECORDER_PROCESS_NAME}
 
-# Also exit network recording command. N.B. the process group (tcpdump ... | split --filter=before_split_cript) has to be exited by
-# killing tcpdump first so that `before_split_cript` can finish and flush buffers.
 pkill -2 gulp
 sleep 10
 pkill -2 gulp
+sleep 10
+pkill -9 gulp
