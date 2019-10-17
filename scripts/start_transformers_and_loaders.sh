120cd6b2c28e32c22bd7bd94c1ec3e86ca0745ef
Jernej Makovsek <jernej.makovsek@gmail.com>
2019-10-17 10:53:38 +0200

    Remove commented out code

--- scripts/start_transformers_and_loaders.sh
+++ scripts/start_transformers_and_loaders.sh
@@ -6,6 +6,4 @@ source ~/.profile
 /opt/puma/puma-ETL/venv/bin/python3 /opt/puma/puma-ETL/src/transformer_pcap.py &
 /opt/puma/puma-ETL/venv/bin/python3 /opt/puma/puma-ETL/src/transformer_msgstorage.py &
 /opt/puma/puma-ETL/venv/bin/python3 /opt/puma/puma-ETL/src/loader_backup.py /mnt/backup/data/puma-recorder &
-#/opt/puma/puma-ETL/venv/bin/python3 /opt/puma/puma-ETL/src/loader_backup.py /home/toaster/staging/backup /home/toaster/staging/loader_backup /home/toaster/staging/transformer_pcap /home/toaster/staging/transformer_msgstorage &
 /opt/puma/puma-ETL/venv/bin/python3 /opt/puma/puma-ETL/src/loader_archive.py /mnt/gdrive/project_data/puma-recorder &
-#/opt/puma/puma-ETL/venv/bin/python3 /opt/puma/puma-ETL/src/loader_archive.py /mnt/gdrive/project_data/puma-recorder /home/toaster/staging/loader_archive /home/toaster/staging/loader_backup &
