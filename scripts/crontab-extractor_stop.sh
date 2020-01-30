680fdf67e1974f74b589eb25ea51e8a55f1196b1
Jernej Makovšek <jernej.makovsek@gmail.com>
2020-01-30 15:49:26 +0100

    Speed up stop script

--- scripts/crontab-extractor_stop.sh
+++ scripts/crontab-extractor_stop.sh
@@ -6,13 +6,7 @@ RECORDER_PROCESS_NAME="puma-recorder"
 # Stop recording and make sure all recording processes are really ended (since they can hang sometimes)
 echo "Killing processes"
 pkill -2 ${RECORDER_PROCESS_NAME}
-sleep 10
+sleep 5
 pkill -2 ${RECORDER_PROCESS_NAME}
-sleep 10
+sleep 2
 pkill -9 ${RECORDER_PROCESS_NAME}
-
-pkill -2 gulp
-sleep 10
-pkill -2 gulp
-sleep 10
-pkill -9 gulp
