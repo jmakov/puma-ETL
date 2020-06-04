d38b9c533ca2872d7b30c791f9e38ad7bd72c940
Jernej Makovšek <jernej.makovsek@gmail.com>
2020-06-04 10:17:40 +0200

    Update staging path

--- scripts/start_transformers_and_loaders.sh
+++ scripts/start_transformers_and_loaders.sh
@@ -2,5 +2,5 @@
 
 /opt/puma-ETL/venv/bin/python3 /opt/puma-ETL/src/transformer_pcap.py &
 /opt/puma-ETL/venv/bin/python3 /opt/puma-ETL/src/transformer_msgstorage.py &
-/opt/puma-ETL/venv/bin/python3 /opt/puma-ETL/src/loader_backup.py /mnt/raid0/data/puma-recorder &
+/opt/puma-ETL/venv/bin/python3 /opt/puma-ETL/src/loader_backup.py /pool_raidz1_3_12TB/data/puma-recorder &
 /opt/puma-ETL/venv/bin/python3 /opt/puma-ETL/src/loader_archive.py /mnt/gdrive/project_data/puma-recorder &
